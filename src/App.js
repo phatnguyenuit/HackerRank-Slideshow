de51efbf1abe1b0b70adfdfff36c06966b5cf32a
phatnt <phatnt@sendo.vn>
2020-09-15 11:30:24 +0700

    Handle logic for slideshow

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import './App.css';
 import 'h8k-components';
 
 import Slides from './components/Slides';
-const title = "Slideshow App";
+const title = 'Slideshow App';
 
-function App({slides}) {
-    return (
-        <div>
-            <h8k-navbar header={title}></h8k-navbar>
-            <div className="App">
-                <Slides slides={slides} />
-            </div>
-        </div>
-    );
+function App({ slides }) {
+  return (
+    <div>
+      <h8k-navbar header={title}></h8k-navbar>
+      <div className="App">
+        <Slides slides={slides} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,21 +1,64 @@
-import React from 'react';
+import React, { useMemo, useCallback, useState } from 'react';
 
-function Slides({slides}) {
+function Slides({ slides }) {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const currentSlide = useMemo(() => slides[selectedIndex], [
+    selectedIndex,
+    slides,
+  ]);
+  const handleNext = useCallback(() => {
+    setSelectedIndex((prevIndex) => {
+      const nextIndex = Math.min(prevIndex + 1, slides.length - 1);
+      return nextIndex;
+    });
+  }, [slides.length]);
+  const handlePrevious = useCallback(() => {
+    setSelectedIndex((prevIndex) => {
+      const previouseIndex = Math.max(prevIndex - 1, 0);
+      return previouseIndex;
+    });
+  }, []);
+  const handleRestart = () => setSelectedIndex(0);
 
-    return (
-        <div>
-            <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
-            </div>
-            <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
-            </div>
-        </div>
-    );
-
+  const disabledNext = useMemo(() => selectedIndex === slides.length - 1, [
+    selectedIndex,
+    slides.length,
+  ]);
+  const disabledPrevious = useMemo(() => selectedIndex === 0, [selectedIndex]);
+  return (
+    <div>
+      <div id="navigation" className="text-center">
+        <button
+          data-testid="button-restart"
+          className="small outlined"
+          disabled={disabledPrevious}
+          onClick={handleRestart}
+        >
+          Restart
+        </button>
+        <button
+          data-testid="button-prev"
+          className="small"
+          disabled={disabledPrevious}
+          onClick={handlePrevious}
+        >
+          Prev
+        </button>
+        <button
+          data-testid="button-next"
+          className="small"
+          disabled={disabledNext}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+      <div id="slide" className="card text-center">
+        <h1 data-testid="title">{currentSlide.title}</h1>
+        <p data-testid="text">{currentSlide.text}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Slides;
